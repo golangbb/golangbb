d536fe013cefc01b6be20472c72a87175d13f653
Anish Patel <anish@ninjacode.co.uk>
2021-03-18 21:03:18 +0000

    added sqlite connection

--- cmd/main.go
+++ cmd/main.go
@@ -4,14 +4,26 @@ import (
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/golangbb/golangbb/v2/pkg/helpers"
+	"gorm.io/gorm"
+	"gorm.io/driver/sqlite"
 )
 
 var (
 	PORT = helpers.GetEnv("PORT", "3000")
+	DATABASE_NAME = helpers.GetEnv("DATABASE_NAME", "golangbb.db")
 )
 
 func init() {
 	fmt.Println("🏗️ initializing golangBB...")
+
+	fmt.Println("💾 connecting to database...")
+	db, err := gorm.Open(sqlite.Open(DATABASE_NAME), &gorm.Config{})
+	if err != nil {
+		panic("💥 failed to connect database")
+	}
+
+	fmt.Println("💾 running database migrations...")
+	db.AutoMigrate()
 }
 
 func main() {
