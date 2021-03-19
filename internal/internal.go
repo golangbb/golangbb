217072e9c730dc5bb56afd6fa428b73093cf4ef0
Anish Patel <anish@ninjacode.co.uk>
2021-03-19 19:27:52 +0000

    fixed default db name

--- internal/internal.go
+++ internal/internal.go
@@ -8,7 +8,7 @@ var (
 	keyPORT             = "PORT"
 	defaultPORT         = "3000"
 	keyDATABASENAME     = "DATABASENAME"
-	defaultDATABASENAME = "golang.db"
+	defaultDATABASENAME = "golangbb.db"
 
 	PORT         = helpers.GetEnv(keyPORT, defaultPORT)
 	DATABASENAME = helpers.GetEnv(keyDATABASENAME, defaultDATABASENAME)
