34b98261faeb34bf3b7a85a8f22a55c9d4f0a747
Anish Patel <anish@ninjacode.co.uk>
2021-03-20 20:51:54 +0000

    moved models migration unit test to models package

--- internal/database/database_test.go
+++ internal/database/database_test.go
@@ -2,8 +2,8 @@ package database
 
 import (
 	"database/sql"
+	"errors"
 	"github.com/DATA-DOG/go-sqlmock"
-	"github.com/golangbb/golangbb/v2/internal/models"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"gorm.io/driver/sqlite"
@@ -67,6 +67,9 @@ var _ = Describe("database", func() {
 				}, gorm.Config{})
 				Expect(err).ShouldNot(HaveOccurred())
 
+				mock.ExpectExec(regexp.QuoteMeta("CREATE TABLE `` ()")).
+					WillReturnError(errors.New("some db error"))
+
 				err = Initialise(struct{}{})
 				Expect(err).Should(HaveOccurred())
 			})
@@ -83,45 +86,6 @@ var _ = Describe("database", func() {
 				err = Initialise()
 				Expect(err).ShouldNot(HaveOccurred())
 				Expect(err).Should(BeNil())
-			})
-		})
-
-		When("initialising with models after connecting", func() {
-			sqlStatements := []string{
-				"CREATE TABLE `users` (`id` integer,`created_at` datetime,`updated_at` datetime,`deleted_at` datetime,`user_name` text,`display_name` text,`password` text NOT NULL,PRIMARY KEY (`id`))",
-				"CREATE UNIQUE INDEX `idx_users_user_name` ON `users`(`user_name`)",
-				"CREATE INDEX `idx_users_deleted_at` ON `users`(`deleted_at`)",
-				"CREATE TABLE `emails` (`email` text,`created_at` datetime,`updated_at` datetime,`deleted_at` datetime,`user_id` integer,PRIMARY KEY (`email`),CONSTRAINT `fk_users_emails` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`))",
-				"CREATE INDEX `idx_emails_deleted_at` ON `emails`(`deleted_at`)",
-				"CREATE TABLE `groups` (`id` integer,`created_at` datetime,`updated_at` datetime,`deleted_at` datetime,`name` text NOT NULL,`author_id` integer,PRIMARY KEY (`id`),CONSTRAINT `fk_groups_author` FOREIGN KEY (`author_id`) REFERENCES `users`(`id`))",
-				"CREATE INDEX `idx_groups_deleted_at` ON `groups`(`deleted_at`)",
-				"CREATE TABLE `users_groups` (`user_id` integer,`group_id` integer,PRIMARY KEY (`user_id`,`group_id`),CONSTRAINT `fk_users_groups_user` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),CONSTRAINT `fk_users_groups_group` FOREIGN KEY (`group_id`) REFERENCES `groups`(`id`))",
-				"CREATE TABLE `topics` (`id` integer,`created_at` datetime,`updated_at` datetime,`deleted_at` datetime,`title` text,`parent_id` integer,PRIMARY KEY (`id`))",
-				"CREATE UNIQUE INDEX `idx_topics_title` ON `topics`(`title`)",
-				"CREATE INDEX `idx_topics_deleted_at` ON `topics`(`deleted_at`)",
-				"CREATE TABLE `discussions` (`id` integer,`created_at` datetime,`updated_at` datetime,`deleted_at` datetime,`author_id` integer,`topic_id` integer,PRIMARY KEY (`id`),CONSTRAINT `fk_discussions_author` FOREIGN KEY (`author_id`) REFERENCES `users`(`id`),CONSTRAINT `fk_discussions_topic` FOREIGN KEY (`topic_id`) REFERENCES `topics`(`id`))",
-				"CREATE INDEX `idx_discussions_deleted_at` ON `discussions`(`deleted_at`)",
-				"CREATE TABLE `posts` (`id` integer,`created_at` datetime,`updated_at` datetime,`deleted_at` datetime,`content` text,`author_id` integer,PRIMARY KEY (`id`),CONSTRAINT `fk_posts_author` FOREIGN KEY (`author_id`) REFERENCES `users`(`id`))",
-				"CREATE INDEX `idx_posts_deleted_at` ON `posts`(`deleted_at`)",
-			}
-			It("should run expected migrations on database", func() {
-				_, err := Connect(sqlite.Dialector{
-					DriverName: "sqlite",
-					Conn:       db,
-				}, gorm.Config{})
-				Expect(err).ShouldNot(HaveOccurred())
-
-				mock.MatchExpectationsInOrder(false)
-				for _, sql := range sqlStatements {
-					migrationSql := regexp.QuoteMeta(sql)
-					mock.ExpectExec(migrationSql).WillReturnResult(sqlmock.NewResult(0, 0))
-				}
-
-				err = Initialise(models.Models()...)
-				Expect(err).ShouldNot(HaveOccurred())
-
-				err = mock.ExpectationsWereMet()
-				Expect(err).ShouldNot(HaveOccurred())
 			})
 		})
 	})
--- internal/models/models_test.go
+++ internal/models/models_test.go
@@ -1,9 +1,14 @@
 package models
 
 import (
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/golangbb/golangbb/v2/internal/database"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"golang.org/x/tools/go/packages"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"regexp"
 	"strings"
 )
 
@@ -41,6 +46,50 @@ var _ = Describe("Models", func() {
 
 			numberOfModels := len(Models())
 			Expect(numberOfModels).Should(Equal(numberOfExportedModels))
+		})
+	})
+
+	Context("Migrations in database.Initialise", func() {
+		When("initialising with models", func() {
+			sqlStatements := []string{
+				"CREATE TABLE `users` (`id` integer,`created_at` datetime,`updated_at` datetime,`deleted_at` datetime,`user_name` text,`display_name` text NOT NULL,`password` text NOT NULL,PRIMARY KEY (`id`))",
+				"CREATE UNIQUE INDEX `idx_users_user_name` ON `users`(`user_name`)",
+				"CREATE INDEX `idx_users_deleted_at` ON `users`(`deleted_at`)",
+				"CREATE TABLE `emails` (`email` text,`created_at` datetime,`updated_at` datetime,`deleted_at` datetime,`user_id` integer,PRIMARY KEY (`email`),CONSTRAINT `fk_users_emails` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`))",
+				"CREATE INDEX `idx_emails_deleted_at` ON `emails`(`deleted_at`)",
+				"CREATE TABLE `groups` (`id` integer,`created_at` datetime,`updated_at` datetime,`deleted_at` datetime,`name` text NOT NULL,`author_id` integer,PRIMARY KEY (`id`),CONSTRAINT `fk_groups_author` FOREIGN KEY (`author_id`) REFERENCES `users`(`id`))",
+				"CREATE INDEX `idx_groups_deleted_at` ON `groups`(`deleted_at`)",
+				"CREATE TABLE `users_groups` (`user_id` integer,`group_id` integer,PRIMARY KEY (`user_id`,`group_id`),CONSTRAINT `fk_users_groups_user` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),CONSTRAINT `fk_users_groups_group` FOREIGN KEY (`group_id`) REFERENCES `groups`(`id`))",
+				"CREATE TABLE `topics` (`id` integer,`created_at` datetime,`updated_at` datetime,`deleted_at` datetime,`title` text,`parent_id` integer,PRIMARY KEY (`id`))",
+				"CREATE UNIQUE INDEX `idx_topics_title` ON `topics`(`title`)",
+				"CREATE INDEX `idx_topics_deleted_at` ON `topics`(`deleted_at`)",
+				"CREATE TABLE `discussions` (`id` integer,`created_at` datetime,`updated_at` datetime,`deleted_at` datetime,`author_id` integer,`topic_id` integer,PRIMARY KEY (`id`),CONSTRAINT `fk_discussions_author` FOREIGN KEY (`author_id`) REFERENCES `users`(`id`),CONSTRAINT `fk_discussions_topic` FOREIGN KEY (`topic_id`) REFERENCES `topics`(`id`))",
+				"CREATE INDEX `idx_discussions_deleted_at` ON `discussions`(`deleted_at`)",
+				"CREATE TABLE `posts` (`id` integer,`created_at` datetime,`updated_at` datetime,`deleted_at` datetime,`content` text,`author_id` integer,PRIMARY KEY (`id`),CONSTRAINT `fk_posts_author` FOREIGN KEY (`author_id`) REFERENCES `users`(`id`))",
+				"CREATE INDEX `idx_posts_deleted_at` ON `posts`(`deleted_at`)",
+			}
+			It("should run expected migrations on database", func() {
+				db, mock, err := sqlmock.New()
+				Expect(err).ShouldNot(HaveOccurred())
+
+				_, err = database.Connect(sqlite.Dialector{
+					DriverName: "sqlite",
+					Conn:       db,
+				}, gorm.Config{})
+				Expect(err).ShouldNot(HaveOccurred())
+
+				mock.MatchExpectationsInOrder(false)
+				for _, sql := range sqlStatements {
+					migrationSql := regexp.QuoteMeta(sql)
+					mock.ExpectExec(migrationSql).WillReturnResult(sqlmock.NewResult(0, 0))
+				}
+
+				err = database.Initialise(Models()...)
+				Expect(err).ShouldNot(HaveOccurred())
+
+				err = mock.ExpectationsWereMet()
+				Expect(err).ShouldNot(HaveOccurred())
+			})
 		})
 	})
 })
