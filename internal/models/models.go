e4ac01baf42e7dc2228c97b9382a8aca1a1e354b
Anish Patel <anish@ninjacode.co.uk>
2021-03-21 19:05:06 +0000

    implemented CreateGroup

--- internal/models/models.go
+++ internal/models/models.go
@@ -5,6 +5,7 @@ import "errors"
 var ErrEmptyUserID = errors.New("empty UserID/AuthorID not allowed")
 var ErrEmptyUserName = errors.New("empty UserName not allowed")
 var ErrEmptyPassword = errors.New("empty Password not allowed")
+var ErrEmptyName = errors.New("empty Name not allowed")
 
 func Models() []interface{} {
 	return []interface{}{
