13a2b435cce571d0bc327da2eae7b66ac77b0135
Anish Patel <anish@ninjacode.co.uk>
2021-04-04 13:48:19 +0100

    implemented create Post

--- internal/models/post.go
+++ internal/models/post.go
@@ -1,6 +1,10 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"github.com/golangbb/golangbb/v2/internal/database"
+	"gorm.io/gorm"
+	"log"
+)
 
 type Post struct {
 	gorm.Model
@@ -9,4 +13,33 @@ type Post struct {
 	AuthorID     uint       `gorm:"not null"`
 	Discussion   Discussion `gorm:"foreignKey:DiscussionID"`
 	DiscussionID uint       `gorm:"not null"`
+}
+
+func CreatePost(post *Post) error {
+	if post.Content == "" {
+		return ErrEmptyContent
+	}
+
+	if post.AuthorID == 0 {
+		return ErrEmptyUserID
+	}
+
+	if post.DiscussionID == 0 {
+		return ErrEmptyDiscussionID
+	}
+
+	err := database.DBConnection.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Omit("Author", "Discussion").Create(post).Error; err != nil {
+			log.Println("[CREATE_POST]::DB_INSERT_POST_ERROR 💥")
+			return err
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
---
+++ internal/models/post_test.go
@@ -0,0 +1,131 @@
+package models
+
+import (
+	"database/sql"
+	"errors"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/golangbb/golangbb/v2/internal/database"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"regexp"
+)
+
+var _ = Describe("Post", func() {
+	var mock sqlmock.Sqlmock
+	var db *sql.DB
+
+	BeforeEach(func() {
+		sqlDb, sqlMock, err := sqlmock.New()
+		Expect(err).ShouldNot(HaveOccurred())
+
+		mock = sqlMock
+		db = sqlDb
+
+		gormDB, err := database.Connect(sqlite.Dialector{
+			DriverName: "sqlite",
+			Conn:       db,
+		}, gorm.Config{})
+		Expect(err).ShouldNot(HaveOccurred())
+		Expect(gormDB).ShouldNot(BeNil())
+		Expect(gormDB.DB()).Should(BeIdenticalTo(db))
+	})
+	AfterEach(func() {
+		db.Close()
+	})
+
+	Context("CreatePost", func() {
+		When("inserting a Post", func() {
+			It("should insert a new Post record with an Author an a Discussion", func() {
+				post := &Post{
+					AuthorID: 10,
+					DiscussionID: 5,
+					Content: "Marvel rules, DC drools",
+				}
+
+				mock.ExpectBegin()
+				mock.ExpectExec(regexp.QuoteMeta("INSERT INTO `posts` (`created_at`,`updated_at`,`deleted_at`,`content`,`author_id`,`discussion_id`) VALUES (?,?,?,?,?,?)")).
+					WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), nil, post.Content, post.AuthorID, post.DiscussionID).
+					WillReturnResult(sqlmock.NewResult(0, 1))
+				mock.ExpectCommit()
+
+				err := CreatePost(post)
+				Expect(err).ShouldNot(HaveOccurred())
+
+				err = mock.ExpectationsWereMet()
+				Expect(err).ShouldNot(HaveOccurred())
+			})
+		})
+
+		When("inserting a Post without an AuthorID", func() {
+			It("should not attempt to insert a new Post record and return an error", func() {
+				post := &Post{
+					DiscussionID: 5,
+					Content: "Marvel rules, DC drools",
+				}
+
+				err := CreatePost(post)
+				Expect(err).Should(HaveOccurred())
+				Expect(err).Should(Equal(ErrEmptyUserID))
+
+				err = mock.ExpectationsWereMet()
+				Expect(err).ShouldNot(HaveOccurred())
+			})
+		})
+
+		When("inserting a Post without a DiscussionID", func() {
+			It("should not attempt to insert a new Post record and return an error", func() {
+				post := &Post{
+					AuthorID: 10,
+					Content: "Marvel rules, DC drools",
+				}
+
+				err := CreatePost(post)
+				Expect(err).Should(HaveOccurred())
+				Expect(err).Should(Equal(ErrEmptyDiscussionID))
+
+				err = mock.ExpectationsWereMet()
+				Expect(err).ShouldNot(HaveOccurred())
+			})
+		})
+
+		When("inserting a Post without Content", func() {
+			It("should not attempt to insert a new Post record and return an error", func() {
+				post := &Post{
+					AuthorID: 10,
+					DiscussionID: 5,
+				}
+
+				err := CreatePost(post)
+				Expect(err).Should(HaveOccurred())
+				Expect(err).Should(Equal(ErrEmptyContent))
+
+				err = mock.ExpectationsWereMet()
+				Expect(err).ShouldNot(HaveOccurred())
+			})
+		})
+
+		When("inserting a Post that errors", func() {
+			It("should rollback transaction and return error", func() {
+				post := &Post{
+					AuthorID: 10,
+					DiscussionID: 5,
+					Content: "Marvel rules, DC drools",
+				}
+
+				mock.ExpectBegin()
+				mock.ExpectExec(regexp.QuoteMeta("INSERT INTO `posts` (`created_at`,`updated_at`,`deleted_at`,`content`,`author_id`,`discussion_id`) VALUES (?,?,?,?,?,?)")).
+					WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), nil, post.Content, post.AuthorID, post.DiscussionID).
+					WillReturnError(errors.New("some db error"))
+				mock.ExpectRollback()
+
+				err := CreatePost(post)
+				Expect(err).Should(HaveOccurred())
+
+				err = mock.ExpectationsWereMet()
+				Expect(err).ShouldNot(HaveOccurred())
+			})
+		})
+	})
+})
